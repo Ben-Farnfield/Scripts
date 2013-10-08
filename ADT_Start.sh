5844f4ca539fda5edc90fb26e5553258ae870d17
mooli <mooli@box.(none)>
2013-10-08 08:00:56 +0100

    added script to fix OS issue and updated ADT start script to include device popup

--- ADT_Start.sh
+++ ADT_Start.sh
@@ -6,7 +6,10 @@ export ADB_HOME=$ADT_HOME/sdk/platform-tools
 # Restart adb server with root privileges.
 $ADB_HOME/adb kill-server
 gksudo $ADB_HOME/adb start-server
-$ADB_HOME/adb devices
+
+# Display devices now attached.
+export DEVICES=$($ADB_HOME/adb devices)
+zenity --info --title="Device List" --text "$DEVICES"
 
 # Start ADT.
 $ADT_HOME/eclipse/eclipse
---
+++ auto-repeat_off.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# bash from autostart.sh to fix volume key bug
+
+terminator --command="xset -r"
+
+exit 0
