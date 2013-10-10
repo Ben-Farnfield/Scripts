5fcf1838d34707f694770a81909fb53e685035ab
Ben-Farnfield <ben.farnfield@gmail.com>
2013-10-10 10:10:33 +0100

    changed set_PATH.sh so it now appends PATH to ~/.profile OR removes PATH from ~/.profile

--- set_PATH.sh
+++ set_PATH.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 
-# Add Java to PATH
-export JAVA_HOME=/usr/local/java/jdk1.7.0_40/bin
-export PATH=$JAVA_HOME:$PATH
+#-----------------------------------------------------------#
+#     This script sets or removes custom PATH variables.    #
+#-----------------------------------------------------------#
 
-# Add ADT tools to PATH
-export ADT_SDK=/usr/local/ADT/sdk
-export P_TOOLS=$ADT_SDK/platform-tools
-export TOOLS=$ADT_SDK/tools
-export PATH=$PATH:$P_TOOLS:$TOOLS
+#-----------------------------------------------------------#
+# WARNING! if you've prev set your path using this script   #
+#          you MUST run this script to remove your old PATH # 
+#          BEFORE making any changes to the PATH variables. #
+#-----------------------------------------------------------#
+
+# Set Your PATH Variables -----------------------------------
+
+# Java
+JAVA_VER=jdk1.7.0_40
+JAVA_HOME=/usr/local/java/$JAVA_VER/bin
+PATH=$JAVA_HOME:$PATH
+
+# ADT tools
+ADT_SDK=/usr/local/ADT/sdk
+P_TOOLS=$ADT_SDK/platform-tools
+TOOLS=$ADT_SDK/tools
+PATH=$PATH:$P_TOOLS:$TOOLS
+
+
+# Add / Remove PATH Variables from ~/.profile ---------------
+
+COMMENT="# set $USER's PATH"
+MSG="You must now log out for your PATH to be set"
+
+# check if PATH prev set by this script
+PATH_SET=$(egrep "^$COMMENT" ~/.profile)
+
+if [ "X$PATH_SET" = "X" ]; then
+	# add PATH to ~/.profile
+	printf "$COMMENT\n" >> ~/.profile
+	printf "PATH=$PATH" >> ~/.profile
+	zenity --info --title="Set PATH" --text "$MSG"
+else
+	# remove PATH from ~/.profile
+	egrep -v "^$COMMENT" ~/.profile > ~/.profile.tmp
+	egrep -v "PATH=$PATH" ~/.profile.tmp > ~/.profile
+	rm ~/.profile.tmp
+	zenity --info --title="Set PATH" --text "$MSG"
+fi
 
 exit 0
