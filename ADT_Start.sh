19f492bb8708baed103aa1b0e36674a2ea2290f6
mooli <mooli@box.(none)>
2013-10-10 11:12:43 +0100

    removed exports from ADT_Start.sh

--- ADT_Start.sh
+++ ADT_Start.sh
@@ -1,17 +1,15 @@
 #! /bin/bash
 
-export ADT_HOME=/usr/local/ADT
-export ADB_HOME=$ADT_HOME/sdk/platform-tools
+ADT_HOME=/usr/local/ADT
+ADB_HOME=$ADT_HOME/sdk/platform-tools
 
 # Restart adb server with root privileges.
 $ADB_HOME/adb kill-server
 gksudo $ADB_HOME/adb start-server
 
 # Display devices now attached.
-export DEVICES=$($ADB_HOME/adb devices)
+DEVICES=$($ADB_HOME/adb devices)
 zenity --info --title="Device List" --text "$DEVICES"
 
 # Start ADT.
 $ADT_HOME/eclipse/eclipse
-
-exit 0
