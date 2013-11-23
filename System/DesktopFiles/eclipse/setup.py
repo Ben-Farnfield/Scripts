a449832f3830c42003a55077b4572fb94325e455
Ben Farnfield <ben.farnfield@gmail.com>
2013-11-23 06:23:59 +0000

    re-factored get_desktop_contents() to make it more readable

--- System/DesktopFiles/eclipse/setup.py
+++ System/DesktopFiles/eclipse/setup.py
@@ -6,33 +6,30 @@ import os
 
 # CONFIG -----------------------------------------------------------------------
 
-USER = "mooli"   # enter your user name
-PROG = "eclipse" # enter program name
-HOME = ("/home/"+USER+"/Scripts/System/DesktopFiles/"+PROG)
+PROG = "eclipse"     # enter program name
+COMMENT = "Java IDE" # enter comment
 
 # Icon
 ICON_SIZES = ["48x48", "64x64"]
 ICON_TYPES = ["png", "svg"]
 ICON_INST_HOME = "/usr/share/icons/hicolor"
 
-# .desktop
-EXEC = "/opt/eclipse/eclipse" # enter execution instructions
-MENU = "Development"          # enter menu entry type
-
 
 # .desktop ---------------------------------------------------------------------
 
+EXEC = "/opt/eclipse/eclipse" # enter execution instructions
+CAT = "Development"           # enter menu entry type
+
 def get_desktop_contents(args):
-    return ('''[Desktop Entry]
-Type=Application
-Encoding=UTF-8
-Name={}
-Comment={}
-Exec={}
-Icon={}/64x64/apps/{}.{}
-Terminal=false
-Categories={};'''
-.format(PROG, PROG, EXEC, ICON_INST_HOME, PROG, args.install, MENU))
+    return ("[Desktop Entry]\n"+
+            "Type=Application\n"+
+            "Encoding=UTF-8\n"+
+            "Name="+PROG+"\n"+
+            "Comment="+COMMENT+"\n"+
+            "Exec="+EXEC+"\n"+
+            "Icon="+ICON_INST_HOME+"/64x64/apps/"+PROG+"."+args.install+"\n"+
+            "Terminal=false"
+            "Categories="+CAT+";")
 
 
 # ------------------------------------------------------------------------------
@@ -46,23 +43,23 @@ def get_args():
     parser.add_argument("-r", "--remove", action="store_true",
                         help="remove .desktop file and icons")
     return parser.parse_args()
-
+ 
 
 def install_icons(args):
     icon_name = (PROG+"."+args.install)
     for i in range(len(ICON_SIZES)):
-        print ("Installing "+ICON_SIZES[i]+" "+icon_name+" icon...")
-        shutil.copyfile((HOME+"/icons/"+ICON_SIZES[i]+"/"+icon_name), 
+        print ("Installing "+ICON_SIZES[i]+" "+icon_name)
+        shutil.copyfile((WRK_DIR+"/icons/"+ICON_SIZES[i]+"/"+icon_name), 
                         (ICON_INST_HOME+"/"+ICON_SIZES[i]+"/apps/"+icon_name))
 
 
 def remove_icons():
     for i in range(len(ICON_SIZES)):
-        print "Removing "+ICON_SIZES[i]+" icons"
         for j in range(len(ICON_TYPES)):
             icon_path = (ICON_INST_HOME+"/"+ICON_SIZES[i]+"/apps/"
                          +PROG+"."+ICON_TYPES[j])
             if os.path.isfile(icon_path):
+                print ("Removing "+ICON_SIZES[i]+" "+icon_name)
                 os.remove(icon_path)
 
 
@@ -74,6 +71,8 @@ def remove_desktop():
     print "Removing .desktop file"
 
 # ------------------------------------------------------------------------------
+
+WRK_DIR = os.getcwd()
 
 args = get_args()
 
